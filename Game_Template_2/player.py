91d7913050deb64876960ac1c6fe409edc4e04d0
Sean Tomlinson <sean.wtomlinson@talktalk.net>
2016-10-11 10:30:01 +0100

    Uploaded both game templates.

---
+++ Game_Template_2/player.py
@@ -0,0 +1,9 @@
+from items import *
+from map import rooms
+
+inventory = [item_id, item_laptop, item_money]
+
+mass = item_id["mass"] + item_laptop["mass"] + item_money["mass"]
+
+# Start game at the reception
+current_room = rooms["Reception"]
